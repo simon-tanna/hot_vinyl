18729fed7795beb6d6c4c8c4846a4750bdbcd5c8
simon-tanna <gcab022109@coderacademy.edu.au>
2022-03-10 09:12:38 +1000

    removed order model

--- app/models/order.rb
+++
@@ -1,6 +0,0 @@
-class Order < ApplicationRecord
-  belongs_to :user
-  belongs_to :product
-  has_one :user
-  has_many :products
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :products
   devise_for :users
   root to: "home#index"
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
