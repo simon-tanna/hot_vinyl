f82ebe45dd2501c8e833069a75b029d1ed992181
simon-tanna <gcab022109@coderacademy.edu.au>
2022-03-15 21:11:32 +1000

    added price numericality validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,7 +6,7 @@ class Product < ApplicationRecord
   has_one :review
   # Ensures fields essential to the operation of the application are present
   validates :name, :artist, :price, :vinyl_weight, :catalog_number, presence: true
+  validates :price, numericality: true
   validates :vinyl_weight, numericality: { in: 120..220 }
-
 
 end
