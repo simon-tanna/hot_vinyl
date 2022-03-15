d22daec737b26fa538b8addefef9317052e6f47d
simon-tanna <gcab022109@coderacademy.edu.au>
2022-03-15 17:09:52 +1000

    added validations for reviews controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :product
   belongs_to :user
   validates :comment, :rating, presence: true
+  validates :comment, length: { in: 15..200 }
+  validates :rating, numericality: { only_integer: true }
 end
