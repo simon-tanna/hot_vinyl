3bfae854e6c60cc08c8618b2e025cad442b341df
simon-tanna <gcab022109@coderacademy.edu.au>
2022-03-17 13:16:42 +1000

    correctly formatted categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+User.destroy_all
+Product.destroy_all
 
-c1 = Category.create(name: 'Rock')
-c2 = Category.create(name: 'Pop')
-c3 = Category.create(name: 'Metal')
-c4 = Category.create(name: 'Alternative')
-c5 = Category.create(name: 'Jazz')
-c6 = Category.create(name: 'World')
-c7 = Category.create(name: 'Classical')
-c8 = Category.create(name: 'Country')
+
+Category.create([{
+        name: 'Rock'
+    },
+    {
+        name: 'Pop'
+    },
+    {
+        name: 'Metal'
+    },
+    {
+        name: 'Alternative'
+    },
+    {
+        name: 'Jazz'
+    },
+    {
+        name: 'World'
+    },
+    {
+        name: 'Classical'
+    },
+    {
+        name: 'Country'
+    },
+    {
+        name: 'Pop'
+    },
+    {
+        name: 'Electronic'
+    }])
+
+
 u1 = User.create(email: 'user@test.com', password: '123123', password_confirmation: '123123', username: 'r_waters', seller: true)
 a1 = Product.create(name: 'Dark Side of the Moon', artist: 'Pink Floyd', price: 300, vinyl_weight: 180, catalog_number: 'CA 5887', condition: 'Average', category_id: 1, user_id: 1, sold_status: false)
 a2 = Product.create(name: 'Animals', artist: 'Pink Floyd', price: 300, vinyl_weight: 180, catalog_number: 'CA 5887', condition: 'Average', category_id: 1, user_id: 1, sold_status: false)
