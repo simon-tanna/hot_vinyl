1a53e7d4ab51e0aee0acccb127d9284292938086
simon-tanna <gcab022109@coderacademy.edu.au>
2022-03-17 11:19:28 +1000

    added sort function to views in user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user_profile
-
+  helper_method :sort_column, :sort_direction
   # Renders user profile on page
   def show
   end
@@ -9,7 +9,16 @@ class UsersController < ApplicationController
 
   def set_user_profile
     @user = User.find(params[:id])
-    @products = @user.products
+    @products = @user.products.order(sort_column + ' ' + sort_direction)
   end
 
+  # These methods define the parameters used by the sorting helper method
+  def sort_column
+    Product.column_names.include?(params[:sort]) ? params[:sort] : 'name'
+  end
+  
+      def sort_direction
+        %w[asc desc].include?(params[:direction]) ? params[:direction] : 'asc'
+      end
+  
 end
