611e69a3cf1f0f36cee7071d63975c41fadc9bce
simon-tanna <gcab022109@coderacademy.edu.au>
2022-03-17 13:47:55 +1000

    created seeds for users and albums

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,10 +35,10 @@ Category.create([{
         name: 'Country'
     },
     {
-        name: 'Pop'
+        name: 'Electronic'
     },
     {
-        name: 'Electronic'
+        name: 'Blues'
     }])
 
 
@@ -71,7 +71,228 @@ User.create([{
         seller: false,
         admin: true
     }])
-a1 = Product.create(name: 'Dark Side of the Moon', artist: 'Pink Floyd', price: 300, vinyl_weight: 180, catalog_number: 'CA 5887', condition: 'Average', category_id: 1, user_id: 1, sold_status: false)
-a2 = Product.create(name: 'Animals', artist: 'Pink Floyd', price: 300, vinyl_weight: 180, catalog_number: 'CA 5887', condition: 'Average', category_id: 1, user_id: 1, sold_status: false)
-a3 = Product.create(name: 'Powerslave', artist: 'Iron Maiden', price: 300, vinyl_weight: 180, catalog_number: 'CA 5887', condition: 'Average', category_id: 1, user_id: 1, sold_status: false)
-a4 = Product.create(name: 'Heartlight', artist: 'Neil Diamond', price: 300, vinyl_weight: 180, catalog_number: 'CA 5887', condition: 'Average', category_id: 1, user_id: 1, sold_status: false)
+    
+Product.create([{
+        name: 'Dark Side of the Moon',
+        artist: 'Pink Floyd',
+        price: 300,
+        vinyl_weight: 180,
+        catalog_number: 'CA 5887',
+        condition: 'Average',
+        category_id: 1,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'Animals',
+        artist: 'Pink Floyd',
+        price: 120,
+        vinyl_weight: 180,
+        catalog_number: 'TB 453003',
+        condition: 'Average',
+        category_id: 1,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'Powerslave',
+        artist: 'Iron Maiden',
+        price: 200,
+        vinyl_weight: 170,
+        catalog_number: 'AU 43929',
+        condition: 'Average',
+        category_id: 3,
+        user_id: 2,
+        sold_status: false
+    },
+    {
+        name: 'Scenes From A Memory',
+        artist: 'Dream Theater',
+        price: 100,
+        vinyl_weight: 180,
+        catalog_number: 'DS 439529',
+        condition: 'Average',
+        category_id: 3,
+        user_id: 2,
+        sold_status: false
+    },
+    {
+        name: 'Waterloo',
+        artist: 'ABBA',
+        price: 40,
+        vinyl_weight: 170,
+        catalog_number: 'RS 3422',
+        condition: 'Average',
+        category_id: 2,
+        user_id: 2,
+        sold_status: false
+    },
+    {
+        name: 'If Not For You',
+        artist: 'Olivia Newton-John',
+        price: 40,
+        vinyl_weight: 170,
+        catalog_number: 'RS 34223',
+        condition: 'Average',
+        category_id: 2,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'The Velvet Underground',
+        artist: 'The Velvet Underground',
+        price: 1200,
+        vinyl_weight: 180,
+        catalog_number: 'CL 39422',
+        condition: 'Average',
+        category_id: 4,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'OK Computer',
+        artist: 'Radiohead',
+        price: 200,
+        vinyl_weight: 180,
+        catalog_number: 'CL 349422',
+        condition: 'Average',
+        category_id: 4,
+        user_id: 2,
+        sold_status: false
+    },
+    {
+        name: 'Kind Of Blue',
+        artist: 'Miles Davis',
+        price: 4000,
+        vinyl_weight: 200,
+        catalog_number: 'RB 39222',
+        condition: 'Average',
+        category_id: 5,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'Bitches Brew',
+        artist: 'Miles Davis',
+        price: 340,
+        vinyl_weight: 200,
+        catalog_number: 'RB 3654222',
+        condition: 'Average',
+        category_id: 5,
+        user_id: 2,
+        sold_status: false
+    },
+    {
+        name: 'Zombie',
+        artist: 'Fela Kuti',
+        price: 220,
+        vinyl_weight: 170,
+        catalog_number: 'EO 32433',
+        condition: 'Average',
+        category_id: 6,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'Zen Kiss',
+        artist: 'Sheila Chandra',
+        price: 135,
+        vinyl_weight: 160,
+        catalog_number: 'RO 455993',
+        condition: 'Average',
+        category_id: 6,
+        user_id: 2,
+        sold_status: false
+    },
+    {
+        name: 'Gershwin: Rhapsody in Blue',
+        artist: 'Andre Previn and the London Symphony Orchestra',
+        price: 20   ,
+        vinyl_weight: 160,
+        catalog_number: 'AL 4022',
+        condition: 'Average',
+        category_id: 7,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'Beethoven: The Complete Piano Sonatas',
+        artist: 'Daniel Barenboim',
+        price: 460,
+        vinyl_weight: 190,
+        catalog_number: 'DB 53003',
+        condition: 'Average',
+        category_id: 7,
+        user_id: 2,
+        sold_status: false
+    },
+    {
+        name: 'Hello Darlin',
+        artist: 'Conway Twitty',
+        price: 15,
+        vinyl_weight: 150,
+        catalog_number: 'CH 3022',
+        condition: 'Average',
+        category_id: 8,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'Jolene',
+        artist: 'Dolly Parton',
+        price: 35,
+        vinyl_weight: 150,
+        catalog_number: 'DO 93883',
+        condition: 'Average',
+        category_id: 8,
+        user_id: 2,
+        sold_status: false
+    },
+    {
+        name: 'Oxygene',
+        artist: 'Jean-Michel Jarre',
+        price: 90,
+        vinyl_weight: 180,
+        catalog_number: 'JS 3952',
+        condition: 'Average',
+        category_id: 9,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'Clic',
+        artist: 'Franco Battiato',
+        price: 25,
+        vinyl_weight: 180,
+        catalog_number: 'UT 38423',
+        condition: 'Average',
+        category_id: 9,
+        user_id: 2,
+        sold_status: false
+    },
+    {
+        name: 'Electric Mud',
+        artist: 'Muddy Waters',
+        price: 60,
+        vinyl_weight: 180,
+        catalog_number: 'SO 43343',
+        condition: 'Average',
+        category_id: 10,
+        user_id: 1,
+        sold_status: false
+    },
+    {
+        name: 'The Real Folk Blues',
+        artist: 'Howlin Wolf',
+        price: 60,
+        vinyl_weight: 170,
+        catalog_number: 'SE 433223',
+        condition: 'Average',
+        category_id: 10,
+        user_id: 2,
+        sold_status: false
+    }])
+
+    p "Created #{Category.count} categories"
+    p "Created #{User.count} users"
+    p "Created #{Product.count} products"
