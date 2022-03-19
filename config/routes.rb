179fe1f66e07a4535d66a1b3d2e1dcf444fa6acd
simon-tanna <gcab022109@coderacademy.edu.au>
2022-03-19 13:01:29 +1000

    added comment to routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   root to: "categories#index"
-
+  # route for category list view accessible from navbar
   get '/category_list', to:'categories#category_list', as: 'category_list'
   # routes for seller's listed items and user placed orders
   get '/my_selling_products', to: 'products#my_selling_products', as: 'my_selling_products'
@@ -17,7 +17,6 @@ Rails.application.routes.draw do
   # route for the search function
   get 'search', to: 'products#search'
   get 'users/:id', to: 'users#profile_view', as: 'profie_view'
-  # get 'orders/new', to: 'orders#new'
-  # post 'orders/create', to:'orders#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
