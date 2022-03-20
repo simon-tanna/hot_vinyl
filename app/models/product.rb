84e7da631c4e61fdbbd143a9f9cac7a210df7826
simon-tanna <gcab022109@coderacademy.edu.au>
2022-03-20 13:49:19 +1000

    tidied validates in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,11 +5,10 @@ class Product < ApplicationRecord
   has_one :order
   has_one :review
   # Ensures fields essential to the operation of the application are present
-  validates :name, :artist, :price, :vinyl_weight, :catalog_number, presence: true
+  validates :name, :artist, :price, :vinyl_weight, :catalog_number, :category, presence: true
   validates :name, length: { in: 1..50 }
   validates :artist, length: { in: 1..50 }
   validates :price, numericality: { in: 1..50000 }
   validates :vinyl_weight, numericality: { in: 120..220 }
   validates :catalog_number, length: { in: 4..12 }
-  validates :category, presence: true
 end
